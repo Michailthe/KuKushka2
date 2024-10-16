9ddb2ae3ed748fc837b7ccc40be927ff644b96e7
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-16 23:01:22 +0300

    Добавьте файлы проекта.

---
+++ Кукушка2/Кукушка2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    cout << "Это программа имитирует часы с кукушкой" << endl;
+    int СooCoo = 0;
+    int hour;
+    cin >> hour;
+    if (hour > !0 and hour < 23)
+    {
+        if (hour == 0)
+        {
+            hour = 24;
+        }
+        while (СooCoo < hour) {
+
+            cout << "Ку-ку" << endl;
+            СooCoo++;
+        }
+    }
+}
